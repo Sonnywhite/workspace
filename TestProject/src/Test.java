8c00710e02fe01f86a68c581b80e9892a410fd70
Sonnywhite <sc_king@gmx.de>
2015-03-25 15:55:59 +0100

    Neue Dinge in der Test-Klasse ausprobiert

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -23,6 +23,10 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Test {
+    
+    private static final byte[] IV = {-73, -42, -74, 97, 30, -1, 18, 16, 50, -108, 92, -80, 24, 79, -83, 26};
+    private static final String CHARSET = "UTF-8";
+    
     public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidParameterSpecException, InvalidAlgorithmParameterException {
 	for (int i = 0; i < 10; i++) {
 	    if (i + i > 10)
@@ -52,51 +56,100 @@ public class Test {
 	System.out.println(toBinaryString((byte) 129));
 	System.out.println(Integer.toBinaryString(129));
 	
-	String plain = "Testtext";
-	String plain_key = "Testschlüssel";
+	String plain = "test";
+	String plain2 = "3";
+	String plain3 = "4";
+	String plain_key = "5";
 	
 	System.out.println("\tPlain: \""+plain+"\""); 
-	System.out.println("\tPlain-Binär: "+toBinaryString(plain.getBytes("UTF-8"))+"");
-	System.out.println("\tPlain-Bytes: "+Arrays.toString(plain.getBytes("UTF-8")));
-	System.out.println("\tPlain-Base64: "+Base64.getEncoder().encodeToString(plain.getBytes("UTF-8")));
+	System.out.println("\tPlain-Binär: "+toBinaryString(plain.getBytes(CHARSET)));
+	System.out.println("\tPlain-Bytes: "+Arrays.toString(plain.getBytes(CHARSET)));
+	System.out.println("\tPlain-Base64: "+Base64.getEncoder().encodeToString(plain.getBytes(CHARSET)));
+	System.out.println("\tPlain-Hex: "+toHexString(plain.getBytes(CHARSET)));
 	System.out.println("---------------------------------");
 	
-	byte[] key_bytes = (plain_key).getBytes("UTF-8");
+	System.out.println("\tPlain2: \""+plain2+"\""); 
+	System.out.println("\tPlain2-Binär: "+toBinaryString(plain2.getBytes(CHARSET)));
+	System.out.println("\tPlain2-Bytes: "+Arrays.toString(plain2.getBytes(CHARSET)));
+	System.out.println("\tPlain2-Base64: "+Base64.getEncoder().encodeToString(plain2.getBytes(CHARSET)));
+	System.out.println("\tPlain2-Hex: "+toHexString(plain2.getBytes(CHARSET)));
+	System.out.println("---------------------------------");
+	
+	System.out.println("\tPlain3: \""+plain3+"\""); 
+	System.out.println("\tPlain3-Binär: "+toBinaryString(plain3.getBytes(CHARSET)));
+	System.out.println("\tPlain3-Bytes: "+Arrays.toString(plain3.getBytes(CHARSET)));
+	System.out.println("\tPlain3-Base64: "+Base64.getEncoder().encodeToString(plain3.getBytes(CHARSET)));
+	System.out.println("\tPlain3-Hex: "+toHexString(plain3.getBytes(CHARSET)));
+	System.out.println("---------------------------------");
+	
+	byte[] key_bytes = (plain_key).getBytes(CHARSET);
 	System.out.println("\tKey-Bytes: "+Arrays.toString(key_bytes));
 	MessageDigest sha = MessageDigest.getInstance("SHA-1");
-	key_bytes = sha.digest(key_bytes);
+	key_bytes = sha.digest();
 	System.out.println("\tKey-Bytes (after SHA-1): "+Arrays.toString(key_bytes));
-	key_bytes = Arrays.copyOf(key_bytes, 16); // benutze nur die ersten 128 bit
+	key_bytes = Arrays.copyOf(key_bytes, 24); // benutze nur die ersten 128 bit
 	Key secretKey = new SecretKeySpec(key_bytes, "AES");
 	System.out.println("\tSecretKey-Bytes: "+Arrays.toString(secretKey.getEncoded()));
+	System.out.println("\tSecretKey-Base64: "+Base64.getEncoder().encodeToString(secretKey.getEncoded()));
 	System.out.println("---------------------------------");
 	
 	Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 	cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 	AlgorithmParameters params = cipher.getParameters();
 	byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-	byte[] cipher_bytes = cipher.doFinal(plain.getBytes("UTF-8"));
-	cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+	byte[] cipher_bytes = cipher.doFinal(plain.getBytes(CHARSET));
+	cipher.init(Cipher.ENCRYPT_MODE, secretKey,new IvParameterSpec(IV));
 	AlgorithmParameters params2 = cipher.getParameters();
 	byte[] iv2 = params2.getParameterSpec(IvParameterSpec.class).getIV();
-	byte[] cipher_bytes2 = cipher.doFinal(plain.getBytes("UTF-8"));
+	byte[] cipher_bytes2 = cipher.doFinal(plain.getBytes(CHARSET));
 	
+	System.out.println("\tIV-Bytes: "+Arrays.toString(iv));
+	System.out.println("\tIV-Base64: "+Base64.getEncoder().encodeToString(iv));
 	System.out.println("\tCipher-Binär: "+toBinaryString(cipher_bytes));
 	System.out.println("\tCipher-Bytes: "+Arrays.toString(cipher_bytes));
-	System.out.println("\tCipher2-Bytes: "+Arrays.toString(cipher_bytes2));
 	System.out.println("\tCipher-Base64: "+Base64.getEncoder().encodeToString(cipher_bytes));
+	System.out.println("---------------------------------");
+	
+	System.out.println("\tIV2-Bytes: "+Arrays.toString(iv2));
+	System.out.println("\tIV2-Base64: "+Base64.getEncoder().encodeToString(iv2));
+	System.out.println("\tCipher2-Binär: "+toBinaryString(cipher_bytes2));
+	System.out.println("\tCipher2-Bytes: "+Arrays.toString(cipher_bytes2));	
 	System.out.println("\tCipher2-Base64: "+Base64.getEncoder().encodeToString(cipher_bytes2));
 	System.out.println("---------------------------------");
 	
 	cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
 	byte[] encrypted_bytes = cipher.doFinal(cipher_bytes);
+	System.out.println("\tEncrypted-Binär: "+toBinaryString(encrypted_bytes));
 	System.out.println("\tEncrypted-Bytes: "+Arrays.toString(encrypted_bytes));
 	System.out.println("\tEncrypted-Base64: "+Base64.getEncoder().encodeToString(encrypted_bytes));
 	cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv2));
 	byte[] encrypted_bytes2 = cipher.doFinal(cipher_bytes2);
+	System.out.println("\tEncrypted2-Binär: "+toBinaryString(encrypted_bytes2));
 	System.out.println("\tEncrypted2-Bytes: "+Arrays.toString(encrypted_bytes2));
 	System.out.println("\tEncrypted2-Base64: "+Base64.getEncoder().encodeToString(encrypted_bytes2));
 	
+    }
+    
+    private static String toHexString(byte b) {
+	return String.format("%02x", b);
+    }
+    
+    private static String toHexString(byte[] bytes) {
+	
+	if(bytes==null)
+	    return null;
+	if(bytes.length==0)
+	    return toHexString(new byte[]{0});
+	
+	int iMax = bytes.length-1;
+	StringBuilder sb = new StringBuilder();
+	for(int i=0; ;i++)
+	{
+	    sb.append(toHexString(bytes[i]));
+	    if(iMax==i)
+		return sb.toString();
+//	    sb.append(" ");
+	}
     }
     
     private static String toBinaryString(byte b) {	
